6c80eda0b46d4912c6de7fa29412b7a9f1dcf1cb
Jangchaeyun <zzangsally@naver.com>
2024-07-05 23:26:32 +0900

    0705 DayTaskManange Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { ThemeProvider } from "@mui/material";
 import { darkTheme } from "./theme/darktheme";
+import Navbar from "./Page/Navbar/Navbar";
 
 function App() {
-  return <ThemeProvider theme={darkTheme}>DayTaskManage</ThemeProvider>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
