4bf0e46973439926a31e2ff6b66e25245f452966
Jangchaeyun <zzangsally@naver.com>
2024-07-09 23:30:36 +0900

    0709 Sidebar

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../Sidebar/Sidebar";
 const Home = () => {
   return (
     <div className="lg:flex px-5 lg:px-20 pt-[2.9vh]">
-      <div>
+      <div className="hidden lg:block w-[25vw] relative">
         <Sidebar />
       </div>
       <div className="right-side-part"></div>
--- src/Page/Sidebar/Sidebar.jsx
+++ src/Page/Sidebar/Sidebar.jsx
@@ -1,16 +1,36 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import "./Sidebar.css";
+
+const menu = [
+  { name: "홈", value: "Home", role: ["ROLE_ADMIN", "ROLE_CUSTOMER"] },
+  { name: "완료", value: "DONE", role: ["ROLE_ADMIN", "ROLE_CUSTOMER"] },
+  { name: "진행 중", value: "ASSIGNED", role: ["ROLE_ADMIN"] },
+  { name: "보류 중", value: "PENDING", role: ["ROLE_ADMIN"] },
+  { name: "새로운 할 일 만들기", value: "", role: ["ROLE_ADMIN"] },
+  { name: "알림", value: "NOTIFICATION", role: ["ROLE_CUSTOMER"] },
+];
+
+const role = "ROLE_ADMIN";
 
 const Sidebar = () => {
   return (
-    <div className="min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
+    <div className="card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
       <div className="space-y-5 h-full">
         <div className="flex justify-center">
           <Avatar
+            className="border-2 border-[#c24dd0]"
             sx={{ width: "8rem", height: "8rem" }}
             src="https://flexible.img.hani.co.kr/flexible/normal/970/627/imgdb/original/2024/0315/20240315502867.jpg"
           />
         </div>
+        {menu
+          .filter((item) => item.role.includes(role))
+          .map((item) => (
+            <p className={`py-3 px-5 rounded-full text-center cursor-pointer`}>
+              {item.name}
+            </p>
+          ))}
       </div>
     </div>
   );
